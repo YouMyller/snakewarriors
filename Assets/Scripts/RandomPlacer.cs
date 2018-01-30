c4dbc2dd0a9046f8366c69959adb368888dd28fd
Siukola Henry TTK16SP <henrysiukola@kamk.fi>
2018-01-30 11:11:06 +0200

    random map

--- Assets/Scripts/RandomPlacer.cs
+++ Assets/Scripts/RandomPlacer.cs
@@ -9,8 +9,13 @@ public class RandomPlacer : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
-        wall.transform.position = new Vector3(Random.Range(-43,45),-32, Random.Range(-30, 120));
+        
+        wall.transform.position = new Vector3(Random.Range(-43, 45),-32, Random.Range(-30, 120));
         print(transform.position.x);
+        
+        wall.transform.localScale = new Vector3(Random.Range(5, 25), -2, Random.Range(5, 25));
+
+
     }
 	
 	// Update is called once per frame
