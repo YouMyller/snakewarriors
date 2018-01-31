09b3c7ced92954d47c9e2157c40fa931f4facfbb
Myllylä Juuso TTK17SP <juusomyllyla1@kamk.fi>
2018-01-31 11:21:05 +0200

    Perhaps fixing things

---
+++ Assets/Scripts/HeadCollision.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadCollision : MonoBehaviour {
+
+    public GameObject spawnPosition;
+<<<<<<< HEAD
+    private int colorOne;
+    private int colorTwo;
+    private bool timeToColor;
+    public HeadCollision playerOneHead;
+    private float time;
+
+	// Use this for initialization
+	void Start ()
+    {
+        timeToColor = true;
+
+        if (gameObject.tag == "P1")
+        {
+            colorOne = Random.Range(0, 4);
+            if (colorOne == 0)
+            {
+                gameObject.GetComponent<Renderer>().material.color = Color.green;
+            }
+            if (colorOne == 1)
+            {
+                gameObject.GetComponent<Renderer>().material.color = Color.red;
+            }
+            if (colorOne == 2)
+            {
+                gameObject.GetComponent<Renderer>().material.color = Color.blue;
+            }
+            if (colorOne == 3)
+            {
+                gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+            }
+            if (colorOne == 4)
+            {
+                Color32 pink = new Color32(221, 153, 214, 255);
+                gameObject.GetComponent<Renderer>().material.color = pink;
+            }
+        }
+=======
+    public SnakeTest st;
+    public List<GameObject> bodyPartsNew = new List<GameObject>();
+
+    // Use this for initialization
+    void Start ()
+    {
+        bodyPartsNew = new List<GameObject>(st.bodyParts);
+>>>>>>> c23e222c47b14dd1918404c02f1de7d2c27e7f2c
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+<<<<<<< HEAD
+        TimeToColor();
+    }
+
+    public void TimeToColor()
+    {
+        if (gameObject.tag == "P2" && timeToColor == true)
+        {
+            colorTwo = Random.Range(0, 4);
+            if (colorTwo == 0)
+            {
+                //Green
+                if (playerOneHead.colorOne == 0)
+                {
+                    gameObject.GetComponent<Renderer>().material.color = Color.red;
+                    timeToColor = false;
+                }
+                else
+                {
+                    gameObject.GetComponent<Renderer>().material.color = Color.green;
+                    timeToColor = false;
+                }
+            }
+            if (colorTwo == 1)
+            {
+                //Red
+                if (playerOneHead.colorOne == 1)
+                {
+                    Color32 cyan = new Color32(115, 255, 255, 255);
+                    gameObject.GetComponent<Renderer>().material.color = cyan;
+                    timeToColor = false;
+                }
+                else
+                {
+                    gameObject.GetComponent<Renderer>().material.color = Color.red;
+                    timeToColor = false;
+                }
+            }
+            if (colorTwo == 2)
+            {
+                //Blue
+                if (playerOneHead.colorOne == 2)
+                {
+                    gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+                    timeToColor = false;
+                }
+                else
+                {
+                    Color32 cyan = new Color32(146, 255, 255, 255);
+                    gameObject.GetComponent<Renderer>().material.color = cyan;
+                    timeToColor = false;
+                }
+            }
+            if (colorTwo == 3)
+            {
+                //Yellow
+                if (playerOneHead.colorOne == 3)
+                {
+                    Color32 pink = new Color32(221, 153, 214, 255);
+                    gameObject.GetComponent<Renderer>().material.color = pink;
+                    timeToColor = false;
+                }
+                else
+                {
+                    gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+                    timeToColor = false;
+                }
+            }
+            if (colorTwo == 4)
+            {
+                //Pink
+                if (playerOneHead.colorOne == 4)
+                {
+                    gameObject.GetComponent<Renderer>().material.color = Color.green;
+                    timeToColor = false;
+                }
+                else
+                {
+                    Color32 pink = new Color32(221, 153, 214, 255);
+                    gameObject.GetComponent<Renderer>().material.color = pink;
+                    timeToColor = false;
+                }
+            }
+        }
+=======
+        bodyPartsNew = st.bodyParts;
+>>>>>>> c23e222c47b14dd1918404c02f1de7d2c27e7f2c
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            transform.position = spawnPosition.transform.position;
+<<<<<<< HEAD
+=======
+            bodyPartsNew[bodyPartsNew.Count - 1].tag = "Null";
+            //
+            //Debug.Log("Kamoon, kamoon beibi kamoon");
+>>>>>>> c23e222c47b14dd1918404c02f1de7d2c27e7f2c
+        }
+
+        /*if (collision.gameObject.tag == "BodyPart")
+        {
+            if (collision.gameObject.transform.parent.tag == "P1" && gameObject.tag == "P2")
+            {
+                //st.points += 1;
+                //st.SetScoreText();
+                //st.scoreText.text = "P1: " + st.points.ToString();
+                Debug.Log("P2 got poits");
+                collision.gameObject.tag = "Null";
+            }
+            if (collision.gameObject.transform.parent.tag == "P2" && gameObject.tag == "P1")
+            {
+                //st.points += 1;
+                //st.SetScoreText();
+                //st.scoreText.text = "P2: " + st.points.ToString();
+                Debug.Log("P2 got poits");
+                collision.gameObject.tag = "Null";
+            }
+        }*/
+    }
+}
---
+++ Assets/Scripts/eatingFood.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class eatingFood : MonoBehaviour {
+
+    public List<GameObject> bodylist = new List<GameObject>();
+
+    public SnakeTest st;
+    public Randomizer random;
+
+    public SnakeTest player1;
+    public SnakeTest player2;
+
+    private bool slowed = false;
+
+    public GameObject end1;
+    public GameObject end2;
+
+     
+
+    // Use this for initialization
+    void Start ()
+    {
+        bodylist = new List<GameObject>(st.bodyParts);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        bodylist = st.bodyParts;
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Food"))
+        {
+            if (random.badfood == false)
+            {
+                st.growTwo = true;
+            }
+            else if (random.badfood == true)
+            {
+                if (bodylist.Count > 1)
+                {
+                    bodylist[bodylist.Count - 1].tag = "Null";
+
+                }
+            }
+        }
+
+        if (collision.gameObject.CompareTag("SuperFood"))
+        {
+            if (random.badfood == false)
+            {
+                st.growFive = true;
+            }
+            else if (random.badfood == true)
+            {
+                if (bodylist.Count > 2)
+                {
+                    bodylist[bodylist.Count - 1].tag = "Null";
+                    bodylist[bodylist.Count - 2].tag = "Null";
+
+                }
+            }
+        }
+
+        if (collision.gameObject.tag == "Powerup")
+        {
+            random.newpower = true;
+        }
+    }
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Grass" && slowed == false)
+        {
+            st.speed = st.speed / 4;
+            slowed = true;
+        }
+        if (collision.gameObject.tag == "Finale")
+        {
+            if (player1.points > player2.points)
+            {
+                end1.SetActive(true);
+            }
+            if (player2.points > player1.points)
+            {
+                end2.SetActive(true);
+            }
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Grass" && slowed == true)
+        {
+            st.speed = st.speed * 4;
+            slowed = false;
+        }
+    }
+}
