89f560b26a5a2ca90d238c0f739fad41689cf74b
Siukola Henry TTK16SP <henrysiukola@kamk.fi>
2018-02-20 16:00:10 +0200

    wohoo working map script

--- Assets/Scripts/RandomPlacer.cs
+++ Assets/Scripts/RandomPlacer.cs
@@ -8,10 +8,10 @@ public class RandomPlacer : MonoBehaviour {
     public GameObject food;
     public GameObject spawnPoint;
     public GameObject spawnPoint2;
-    public Vector3 radius;
-    public Collider[] colliders;
     public int wallAmount;
     public int foodAmount;
+    
+
 
     // Use this for initialization
     void Start ()
@@ -19,6 +19,7 @@ public class RandomPlacer : MonoBehaviour {
         createWalls();
         createFood();
         playerSpawn();
+        
     }
 	
 	// Update is called once per frame
@@ -33,96 +34,38 @@ public class RandomPlacer : MonoBehaviour {
         for (int i = 0; i < wallAmount; i++)
         {
             Vector3 spawnPos = new Vector3(0, -31, 0);
-            bool canSpawnHere = false;
             wall.transform.localScale = new Vector3(Random.Range(3, 25), 1, Random.Range(3, 25));
-
-
-            while (!canSpawnHere)
-            {
 
                 float spawnPosX = Random.Range(-43.5f, 45.5f);
                 float spawnPosY = Random.Range(-30.5f, 120.5f);
                 spawnPos = new Vector3(spawnPosX, -31, spawnPosY);
-                canSpawnHere = preventSpawnOverlap(spawnPos);
-
-            }
-
                 GameObject seina = Instantiate(wall, spawnPos, Quaternion.identity) as GameObject;
+            
         }
     }
-
+    
         void createFood()
         {
         for (int i = 0; i < foodAmount; i++)
         {
             Vector3 spawnPos = new Vector3(0, -31, 0);
-            bool canSpawnHere = false;
-
-            while (!canSpawnHere)
-            {
-
                 float spawnPosX = Random.Range(-43.5f, 45.5f);
                 float spawnPosY = Random.Range(-30.5f, 120.5f);
                 spawnPos = new Vector3(spawnPosX, -31, spawnPosY);
-                canSpawnHere = preventSpawnOverlap(spawnPos);
-
-
-            }
-
             GameObject ruoka = Instantiate(food, spawnPos, Quaternion.AngleAxis(90, Vector3.left)) as GameObject;
         }
     }
     void playerSpawn()
     {
         Vector3 spawnPos = new Vector3(0, -31, 0);
-        bool canSpawnHere = false;
-
-
-        while (!canSpawnHere)
-        {
-
             float spawnPosX = Random.Range(-43.5f, 45.5f);
             float spawnPosY = Random.Range(-30.5f, 120.5f);
             spawnPos = new Vector3(spawnPosX, -31, spawnPosY);
-            canSpawnHere = preventSpawnOverlap(spawnPos);
-        }
         spawnPoint.transform.position = spawnPos;
         spawnPoint2.transform.position = spawnPos;
 
     }
 
-    bool preventSpawnOverlap(Vector3 spawnPos)
-    {
-        
-        radius = wall.transform.localScale;
-        colliders = Physics.OverlapBox(wall.transform.position, radius);
-        for (int i = 0; i < colliders.Length; i++)
-        {
-            Vector3 centerPoint = colliders[i].bounds.center;
-            float width = colliders[i].bounds.extents.x +10;
-            float height = colliders[i].bounds.extents.z;
 
-            float leftextent = centerPoint.x - width;
-            float rightextent = centerPoint.x + width;
-            float upperextent = centerPoint.z + height;
-            float lowerextent = centerPoint.z - height;
-
-            if (spawnPos.x > leftextent && spawnPos.x < rightextent)
-            {
-                if (spawnPos.z > lowerextent && spawnPos.z < upperextent)
-                {
-
-                    return false;
-                }
-
-            }
-
-
-        }
-
-        return true;
-        
-    }
-    
 
 }
---
+++ Assets/Scripts/spawnCheck.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnCheck : MonoBehaviour {
+
+    public GameObject spawnedObject;
+    public Vector3 radius;
+    public Collider[] colliders;
+
+    // Use this for initialization
+    void Start ()
+    {
+        createWalls();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void createWalls()
+    {
+            Vector3 spawnPos = new Vector3(0, -31, 0);
+            bool canSpawnHere = false;
+
+        int safetynet = 0;
+
+            while (canSpawnHere == false)
+            {
+            spawnedObject.transform.localScale = new Vector3(Random.Range(3, 25), 1, Random.Range(3, 25));
+            float spawnPosX = Random.Range(-43.5f, 45.5f);
+                float spawnPosY = Random.Range(-30.5f, 120.5f);
+                spawnPos = new Vector3(spawnPosX, -31, spawnPosY);
+                canSpawnHere = preventSpawnOverlap(spawnPos);
+            spawnedObject.transform.position = spawnPos;
+            safetynet ++;
+            if (safetynet > 500)
+            {
+                Debug.Log("too many attempts");
+                break;
+            }
+
+            }
+
+
+
+    }
+    
+
+    bool preventSpawnOverlap(Vector3 spawnPos)
+    {
+
+        radius = spawnedObject.transform.localScale;
+        colliders = Physics.OverlapBox(spawnedObject.transform.position, radius);
+        
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            Vector3 centerPoint = colliders[i].bounds.center;
+            float width = colliders[i].bounds.extents.x;
+            float height = colliders[i].bounds.extents.z;
+
+            float leftextent = centerPoint.x - width;
+            float rightextent = centerPoint.x + width;
+            float upperextent = centerPoint.z + height;
+            float lowerextent = centerPoint.z - height;
+
+
+        }
+        
+        if (colliders.Length > 1)
+        {
+            
+            return false;
+        }
+
+            return true;
+
+
+    }
+
+}
